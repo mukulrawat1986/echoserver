73ec626fd9f3ebcce44fa1d6be59671192d1ee6b
mukulrawat1986 <mukulsrawat@gmail.com>
2015-12-14 23:12:34 +0530

    done with client program

---
+++ client/fetch/fetch.go
@@ -0,0 +1,38 @@
+// Simple client to fetch the contents found at a url
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	if len(os.Args) == 1 {
+		fmt.Fprintf(os.Stderr, "Error: Argument missing\n")
+		os.Exit(1)
+	} else {
+		for _, url := range os.Args[1:] {
+
+			if res := strings.HasPrefix(url, "http://"); !res {
+				url = "http://" + url
+			}
+			resp, err := http.Get(url)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+				os.Exit(1)
+			}
+			b, err := ioutil.ReadAll(resp.Body)
+			defer resp.Body.Close()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+				os.Exit(1)
+			}
+			fmt.Printf("%s", b)
+		}
+	}
+}
